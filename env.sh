628778bfe127835085650081199a8a1a78fbfcea
Derek Eclavea <declavea@sonic.net>
2023-03-29 14:52:54 -0700

    Added MAX TRACKS to the envornment script

--- env.sh
+++ env.sh
@@ -2,3 +2,4 @@ export SPOTIPY_CLIENT_ID=[place spotify client id here]
 export SPOTIPY_CLIENT_SECRET=[place spotify secred here]
 export SPOTIPY_REDIRECT_URI=http://localhost/auth.html
 export PBL_CACHE=REDIS
+export MAX_TRACKS=10000
