7b22f77cfbcb5ddf9c1727cb7c3d9368176fe13c
Derek Eclavea <declavea@sonic.net>
2023-03-29 13:17:17 -0700

    Fixed setup_nginx (forgot dirname)

--- nginx/setup_nginx.sh
+++ nginx/setup_nginx.sh
@@ -1,6 +1,6 @@
 # Note, this script requires being run as sudo, in order to copy the confg file int /etc/nginx/conf.d
 
-web_path=$(pwd)"/web"
+web_path=$(dirname "$(pwd)")"/web"
 
 # using ? instead of / in the folling sed since pwd output will also contain back slashes
 sed -e"s?WEB_PATH?$web_path?" ./smarterplaylists > /etc/nginx/conf.d/smarterplaylists
