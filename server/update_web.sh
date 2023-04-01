072c8b4b7db1bdeef757757b43257116800b4ded
Derek Eclavea <declavea@sonic.net>
2023-04-01 10:50:46 -0700

    Modified update_web.js to update the MAX_TRACKS value in web/program.js

--- server/update_web.sh
+++ server/update_web.sh
@@ -7,7 +7,14 @@ web_path=$(dirname "$(pwd)")"/web"
 
 if grep -q "SPOTIPY_CLIENT_ID" "$web_path/main.js"; then
      sed -i "s/SPOTIPY_CLIENT_ID/$SPOTIPY_CLIENT_ID/" $web_path/main.js
-     echo $web_path"/main.js has been updated with your SPOTTIFY_CLIENT_ID environment variable"
+     echo "SUCCESS: "$web_path"/main.js has been updated with your SPOTTIFY_CLIENT_ID environment variable"
 else
-     echo $web_path"/main.js already has the SPOTTIFY_CLIENT_ID value set"
+     echo "SKIPPED: "$web_path"/main.js already has the SPOTTIFY_CLIENT_ID value set"
+fi
+
+if grep -q "MAX_TRACKS" "$web_path/program.js"; then
+     sed -i "s/MAX_TRACKS/$MAX_TRACKS/" $web_path/program.js
+     echo "SUCCESS: "$web_path"/program.js has been updated with your MAX_TRACKS value"
+else
+     echo "SKIPPED: "$web_path"/program.js already has the MAX_TRACKS set"
 fi
